c96fc94b938f219e39332fcf3c13a1ef2c794e2a
Cheng Feng CHOY <choy.cheng.feng@outlook.sg>
2020-02-23 17:13:48 +0800

    Added Player and Shop Class. Currently Shop class does not work yet together with Player. But Player standalone works.

---
+++ DM2122 Prac/Source/Shop.cpp
@@ -0,0 +1,46 @@
+#include "Shop.h"
+
+Shop::Shop()
+{
+	priceOfCar[0] = 20000;
+	priceOfCar[1] = 40000;
+	priceOfCar[2] = 10000;
+
+}
+
+Shop::~Shop()
+{
+}
+
+void Shop::buyCar(int whichCar)
+{
+	if (User.hasCar(whichCar) == false)
+	{
+		User.removeMoney(priceOfCar[whichCar]);
+
+		User.addCar(whichCar);
+
+		std::cout << "You have bought the Car! " << whichCar << std::endl;
+	}
+	else
+	{
+		std::cout << "You already have the car!" << std::endl;
+	}
+}
+
+void Shop::sellCar(int whichCar)
+{
+	if (User.hasCar(whichCar) == true)
+	{
+		User.addMoney(priceOfCar[whichCar]);
+
+		User.removeCar(whichCar);
+		
+		std::cout << "You have successfull sold your Car!" << std::endl;
+	}
+	else
+	{
+		std::cout << "You do not own this car!" << std::endl;
+	}
+
+}
