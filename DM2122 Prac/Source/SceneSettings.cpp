40d6334ae90b6d291138436b31103c9b6d4bfb10
kevin73633 <kevindeepak@hotmail.com>
2020-02-26 18:02:01 +0800

    stuff

---
+++ DM2122 Prac/Source/SceneSettings.cpp
@@ -0,0 +1,503 @@
+#include "SceneSettings.h"
+#include "GL\glew.h"
+#include "Application.h"
+#include <Mtx44.h>
+#include "shader.hpp"
+#include "MeshBuilder.h"
+#include "Utility.h"
+#include "LoadTGA.h"
+
+#define ROT_LIMIT 45.f;
+#define SCALE_LIMIT 5.f;
+#define LSPEED 10.f
+
+SceneSetting::SceneSetting()
+{
+	for (int i = 0; i < NUM_GEOMETRY; ++i)
+	{
+		meshList[i] = NULL;
+	}
+}
+
+SceneSetting::~SceneSetting()
+{
+}
+
+void SceneSetting::Init()
+{
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	// Generate a default VAO for now
+	glGenVertexArrays(1, &m_vertexArrayID);
+	glBindVertexArray(m_vertexArrayID);
+	glEnable(GL_CULL_FACE);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	camera.Init(Vector3(40, 1, 0), Vector3(0, 1, 0), Vector3(0, 1, 0));
+
+	Mtx44 projection;
+	projection.SetToPerspective(45.f, 4.f / 3.f, 0.1f, 1000.f);
+	projectionStack.LoadMatrix(projection);
+
+	//m_programID = LoadShaders("Shader//Texture.vertexshader", "Shader//Blending.fragmentshader");
+
+	m_programID = LoadShaders("Shader//Texture.vertexshader", "Shader//Text.fragmentshader");
+
+	m_parameters[U_MVP] = glGetUniformLocation(m_programID, "MVP");
+	m_parameters[U_MODELVIEW] = glGetUniformLocation(m_programID, "MV");
+	m_parameters[U_MODELVIEW_INVERSE_TRANSPOSE] = glGetUniformLocation(m_programID, "MV_inverse_transpose");
+	m_parameters[U_MATERIAL_AMBIENT] = glGetUniformLocation(m_programID, "material.kAmbient");
+	m_parameters[U_MATERIAL_DIFFUSE] = glGetUniformLocation(m_programID, "material.kDiffuse");
+	m_parameters[U_MATERIAL_SPECULAR] = glGetUniformLocation(m_programID, "material.kSpecular");
+	m_parameters[U_MATERIAL_SHININESS] = glGetUniformLocation(m_programID, "material.kShininess");
+	m_parameters[U_TEXT_ENABLED] = glGetUniformLocation(m_programID, "textEnabled");
+	m_parameters[U_TEXT_COLOR] = glGetUniformLocation(m_programID, "textColor");
+	m_parameters[U_LIGHT0_POSITION] = glGetUniformLocation(m_programID, "lights[0].position_cameraspace");
+	m_parameters[U_LIGHT0_COLOR] = glGetUniformLocation(m_programID, "lights[0].color");
+	m_parameters[U_LIGHT0_POWER] = glGetUniformLocation(m_programID, "lights[0].power");
+	m_parameters[U_LIGHT0_KC] = glGetUniformLocation(m_programID, "lights[0].kC");
+	m_parameters[U_LIGHT0_KL] = glGetUniformLocation(m_programID, "lights[0].kL");
+	m_parameters[U_LIGHT0_KQ] = glGetUniformLocation(m_programID, "lights[0].kQ");
+	m_parameters[U_LIGHTENABLED] = glGetUniformLocation(m_programID, "lightEnabled");
+	m_parameters[U_NUMLIGHTS] = glGetUniformLocation(m_programID, "numLights");
+	m_parameters[U_LIGHT0_TYPE] = glGetUniformLocation(m_programID, "lights[0,1].type");
+	m_parameters[U_LIGHT0_SPOTDIRECTION] = glGetUniformLocation(m_programID, "lights[0].spotDirection");
+	m_parameters[U_LIGHT0_COSCUTOFF] = glGetUniformLocation(m_programID, "lights[0].cosCutoff");
+	m_parameters[U_LIGHT0_COSINNER] = glGetUniformLocation(m_programID, "lights[0].cosInner");
+	m_parameters[U_LIGHT0_EXPONENT] = glGetUniformLocation(m_programID, "lights[0].exponent");
+
+
+
+	//Get a handle for our "colorTexture" uniform
+	m_parameters[U_COLOR_TEXTURE_ENABLED] = glGetUniformLocation(m_programID, "colorTextureEnabled");
+	m_parameters[U_COLOR_TEXTURE] = glGetUniformLocation(m_programID, "colorTexture");
+
+	glUseProgram(m_programID);
+	// Enable depth test
+	glEnable(GL_DEPTH_TEST);
+
+	light[0].type = Light::LIGHT_POINT;
+	light[0].position.Set(0, 5, 0);
+	light[0].color.Set(0.5f, 0.5f, 0.5f);
+	light[0].power = 3;
+	light[0].kC = 1.f;
+	light[0].kL = 0.01f;
+	light[0].kQ = 0.001f;
+	light[0].cosCutoff = cos(Math::DegreeToRadian(45));
+	light[0].cosInner = cos(Math::DegreeToRadian(30));
+	light[0].exponent = 3.f;
+	light[0].spotDirection.Set(0.f, 1.f, 0.f);
+
+
+
+	glUniform1i(m_parameters[U_LIGHT0_TYPE], light[0].type);
+	glUniform3fv(m_parameters[U_LIGHT0_COLOR], 1, &light[0].color.r);
+	glUniform1f(m_parameters[U_LIGHT0_POWER], light[0].power);
+	glUniform1f(m_parameters[U_LIGHT0_KC], light[0].kC);
+	glUniform1f(m_parameters[U_LIGHT0_KL], light[0].kL);
+	glUniform1f(m_parameters[U_LIGHT0_KQ], light[0].kQ);
+	glUniform3fv(m_parameters[U_LIGHT0_SPOTDIRECTION], 1, &light[0].spotDirection.x);
+	glUniform1f(m_parameters[U_LIGHT0_COSCUTOFF], light[0].cosCutoff);
+	glUniform1f(m_parameters[U_LIGHT0_COSINNER], light[0].cosInner);
+	glUniform1f(m_parameters[U_LIGHT0_EXPONENT], light[0].exponent);
+
+
+	glUniform1i(m_parameters[U_NUMLIGHTS], 1);
+
+	meshList[GEO_LEFT] = MeshBuilder::GenerateQuad("left", Color(1, 1, 1), 1.f, 1.f);
+	meshList[GEO_LEFT]->textureID = LoadTGA("Image//left.tga");
+
+	meshList[GEO_RIGHT] = MeshBuilder::GenerateQuad("right", Color(1, 1, 1), 1.f, 1.f);
+	meshList[GEO_RIGHT]->textureID = LoadTGA("Image//right.tga");
+
+	meshList[GEO_TOP] = MeshBuilder::GenerateQuad("top", Color(1, 1, 1), 1.f, 1.f);
+	meshList[GEO_TOP]->textureID = LoadTGA("Image//top.tga");
+
+	meshList[GEO_BOTTOM] = MeshBuilder::GenerateQuad("bottom", Color(1, 1, 1), 1.f, 1.f);
+	meshList[GEO_BOTTOM]->textureID = LoadTGA("Image//bottom.tga");
+
+	meshList[GEO_FRONT] = MeshBuilder::GenerateQuad("front", Color(1, 1, 1), 1.f, 1.f);
+	meshList[GEO_FRONT]->textureID = LoadTGA("Image//front.tga");
+
+	meshList[GEO_BACK] = MeshBuilder::GenerateQuad("back", Color(1, 1, 1), 1.f, 1.f);
+	meshList[GEO_BACK]->textureID = LoadTGA("Image//back.tga");
+
+	meshList[GEO_CHAR] = MeshBuilder::GenerateQuad("char", Color(1, 1, 1), 1.f, 1.f);
+	meshList[GEO_CHAR]->textureID = LoadTGA("Image//char.tga");
+
+	meshList[GEO_SETTINGMENU] = MeshBuilder::GenerateQuad("menu", Color(0.f, 0.63f, 0.48f), 50.f, 50.f);
+
+	meshList[GEO_POINTER] = MeshBuilder::GenerateQuad("pointer", Color(1.f, 1.f, 1.f), 10.f, 10.f);
+
+	meshList[GEO_SETTING] = MeshBuilder::GenerateQuad("setting", Color(1.f, 0.f, 0.f), 20.f, 20.f);
+
+	meshList[GEO_AUDIOBUTTON] = MeshBuilder::GenerateQuad("audiobutton", Color(1, 0, 0), 15.f, 15.f);
+	//meshList[GEO_AUDIOBUTTON]->textureID = LoadTGA("Image//switch.tga");
+
+	meshList[GEO_BUTTON1] = MeshBuilder::GenerateQuad("button1", Color(1, 1, 1), 10.f, 10.f);
+	meshList[GEO_BUTTON1]->textureID = LoadTGA("Image//switchon.tga");
+
+	meshList[GEO_BUTTON2] = MeshBuilder::GenerateQuad("button2", Color(1, 1, 1), 10.f, 10.f);
+	meshList[GEO_BUTTON2]->textureID = LoadTGA("Image//switchon.tga");
+
+	meshList[GEO_BUTTON3] = MeshBuilder::GenerateQuad("button3", Color(1, 1, 1), 10.f, 10.f);
+	meshList[GEO_BUTTON3]->textureID = LoadTGA("Image//switchon.tga");
+
+
+	meshList[GEO_LIGHTSPHERE] = MeshBuilder::GenerateSphere("lightBall", Color(1.f, 1.f, 1.f), 9, 36, 1.f);
+
+	clock = 0;
+	clock2 = 0;
+
+	button1 = true;
+	button2 = true;
+	button3 = true;
+}
+
+void SceneSetting::Update(double dt)
+{
+	clock += dt;
+	clock2 += dt;
+
+	if (Application::IsKeyPressed(0x31))
+	{
+		glDisable(GL_CULL_FACE);
+	}
+	else if (Application::IsKeyPressed(0x32))
+	{
+		glEnable(GL_CULL_FACE);
+	}
+	else if (Application::IsKeyPressed(0x33))
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+	else if (Application::IsKeyPressed(0x34))
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
+	if (Application::IsKeyPressed('I'))
+		light[0].position.z -= (float)(LSPEED * dt);
+	if (Application::IsKeyPressed('K'))
+		light[0].position.z += (float)(LSPEED * dt);
+	if (Application::IsKeyPressed('J'))
+		light[0].position.x -= (float)(LSPEED * dt);
+	if (Application::IsKeyPressed('L'))
+		light[0].position.x += (float)(LSPEED * dt);
+	if (Application::IsKeyPressed('O'))
+		light[0].position.y -= (float)(LSPEED * dt);
+	if (Application::IsKeyPressed('P'))
+		light[0].position.y += (float)(LSPEED * dt);
+
+	if (Application::IsKeyPressed('Q'))
+	{
+		//to do: switch light type to POINT and pass the information to
+		light[0].type = Light::LIGHT_POINT;
+
+	}
+	else if (Application::IsKeyPressed('W'))
+	{
+		//to do: switch light type to DIRECTIONAL and pass the
+		light[0].type = Light::LIGHT_DIRECTIONAL;
+
+	}
+	else if (Application::IsKeyPressed('E'))
+	{
+		//to do: switch light type to SPOT and pass the information to
+		light[0].type = Light::LIGHT_SPOT;
+
+	}
+
+
+	//camera.Update(dt);
+
+	//float height = Application::getmouseYpos();
+	//Application::mouseupdate();
+
+	int offset = 8;
+	if ((Application::IsKeyPressed('S')) && clock < GetTickCount64() && level < 3)
+	{
+		pos -= offset;
+		level++;
+		clock = GetTickCount64() + 500;
+	}
+	else if ((Application::IsKeyPressed('W')) && clock < GetTickCount64() && level > 1)
+	{
+		pos += offset;
+		level--;
+		clock = GetTickCount64() + 500;
+	}
+
+	if (level == 1 && clock2 < GetTickCount64() && (Application::IsKeyPressed(VK_LBUTTON) || Application::IsKeyPressed(VK_RETURN)))
+	{
+		clock2 = GetTickCount64() + 200;
+		/*if (audioOn == true)
+			audioOn = false;
+		if (audioOn == false)
+			audioOn = true;*/
+		if (button1 == true)
+		{
+			meshList[GEO_BUTTON1]->textureID = LoadTGA("Image//switchoff.tga");
+			button1 = false;
+		}
+		else if (button1 == false)
+		{
+			meshList[GEO_BUTTON1]->textureID = LoadTGA("Image//switchon.tga");
+			button1 = true;
+		}
+	}
+	else if (level == 2 && clock2 < GetTickCount64() && (Application::IsKeyPressed(VK_LBUTTON) || Application::IsKeyPressed(VK_RETURN)))
+	{
+		clock2 = GetTickCount64() + 200;
+		if (button2 == true)
+		{
+			meshList[GEO_BUTTON2]->textureID = LoadTGA("Image//switchoff.tga");
+			button2 = false;
+		}
+		else if (button2 == false)
+		{
+			meshList[GEO_BUTTON2]->textureID = LoadTGA("Image//switchon.tga");
+			button2 = true;
+		}
+	}
+	else if (level == 3 && clock2 < GetTickCount64() && (Application::IsKeyPressed(VK_LBUTTON) || Application::IsKeyPressed(VK_RETURN)))
+	{
+		clock2 = GetTickCount64() + 200;
+		if (button3 == true)
+		{
+			meshList[GEO_BUTTON3]->textureID = LoadTGA("Image//switchoff.tga");
+			button3 = false;
+		}
+		else if (button3 == false)
+		{
+			meshList[GEO_BUTTON3]->textureID = LoadTGA("Image//switchon.tga");
+			button3 = true;
+		}
+	}
+}
+
+
+void SceneSetting::RenderText(Mesh * mesh, std::string text, Color color)
+{
+	if (!mesh || mesh->textureID <= 0)
+		return;
+
+	glDisable(GL_DEPTH_TEST);
+
+	glUniform1i(m_parameters[U_TEXT_ENABLED], 1);
+	glUniform3fv(m_parameters[U_TEXT_COLOR], 1, &color.r);
+	glUniform1i(m_parameters[U_LIGHTENABLED], 0);
+	glUniform1i(m_parameters[U_COLOR_TEXTURE_ENABLED], 1);
+
+	glActiveTexture(GL_TEXTURE0);
+	
+	glBindTexture(GL_TEXTURE_2D, mesh->textureID);
+	glUniform1i(m_parameters[U_COLOR_TEXTURE], 0);
+
+	for (unsigned i = 0; i < text.length(); ++i)
+	{
+		Mtx44 characterSpacing;
+		characterSpacing.SetToTranslation(i * 0.6f, 0, 0);
+		Mtx44 MVP = projectionStack.Top() * viewStack.Top() * modelStack.Top() * characterSpacing;
+		glUniformMatrix4fv(m_parameters[U_MVP], 1, GL_FALSE, &MVP.a[0]);
+
+		mesh->Render((unsigned)text[i] * 6, 6);
+	}
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glUniform1i(m_parameters[U_TEXT_ENABLED], 0);
+	glEnable(GL_DEPTH_TEST);
+}
+
+void SceneSetting::RenderTextOnScreen(Mesh * mesh, std::string text, Color color, float size, float x, float y)
+{
+	if (!mesh || mesh->textureID <= 0)
+		return;
+
+	glDisable(GL_DEPTH_TEST);
+
+	Mtx44 ortho;
+	ortho.SetToOrtho(0, 80, 0, 60, -10, 10);
+	projectionStack.PushMatrix();
+	projectionStack.LoadMatrix(ortho);
+	viewStack.PushMatrix();
+	viewStack.LoadIdentity();
+	modelStack.PushMatrix();
+	modelStack.LoadIdentity();
+	modelStack.Scale(size, size, size);
+	modelStack.Translate(x, y, 0);
+
+	glUniform1i(m_parameters[U_TEXT_ENABLED], 1);
+	glUniform3fv(m_parameters[U_TEXT_COLOR], 1, &color.r);
+	glUniform1i(m_parameters[U_LIGHTENABLED], 0);
+	glUniform1i(m_parameters[U_COLOR_TEXTURE_ENABLED], 1);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, mesh->textureID);
+	glUniform1i(m_parameters[U_COLOR_TEXTURE], 0);
+	for (unsigned i = 0; i < text.length(); ++i)
+	{
+		Mtx44 characterSpacing;
+		characterSpacing.SetToTranslation(i * 1.0f, 0, 0);
+		Mtx44 MVP = projectionStack.Top() * viewStack.Top() * modelStack.Top() * characterSpacing;
+		glUniformMatrix4fv(m_parameters[U_MVP], 1, GL_FALSE, &MVP.a[0]);
+
+		mesh->Render((unsigned)text[i] * 6, 6);
+	}
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glUniform1i(m_parameters[U_TEXT_ENABLED], 0);
+	projectionStack.PopMatrix();
+	viewStack.PopMatrix();
+	modelStack.PopMatrix();
+
+	glEnable(GL_DEPTH_TEST);
+
+}
+
+
+void SceneSetting::Render()
+{
+	//Clear color & depth buffer every frame
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	viewStack.LoadIdentity();
+	viewStack.LookAt(camera.position.x, camera.position.y, camera.position.z, camera.target.x, camera.target.y, camera.target.z, camera.up.x, camera.up.y, camera.up.z);
+	modelStack.LoadIdentity();
+
+	// passing the light direction if it is a direction light	
+	if (light[0].type == Light::LIGHT_DIRECTIONAL)
+	{
+		Vector3 lightDir(light[0].position.x, light[0].position.y, light[0].position.z);
+		Vector3 lightDirection_cameraspace = viewStack.Top() * lightDir;
+		glUniform3fv(m_parameters[U_LIGHT0_POSITION], 1, &lightDirection_cameraspace.x);
+	}
+	// if it is spot light, pass in position and direction 
+	else if (light[0].type == Light::LIGHT_SPOT)
+	{
+		Position lightPosition_cameraspace = viewStack.Top() * light[0].position;
+		glUniform3fv(m_parameters[U_LIGHT0_POSITION], 1, &lightPosition_cameraspace.x);
+		Vector3 spotDirection_cameraspace = viewStack.Top() * light[0].spotDirection;
+		glUniform3fv(m_parameters[U_LIGHT0_SPOTDIRECTION], 1, &spotDirection_cameraspace.x);
+	}
+	else
+	{
+		// default is point light (only position since point light is 360 degrees)
+		Position lightPosition_cameraspace = viewStack.Top() * light[0].position;
+		glUniform3fv(m_parameters[U_LIGHT0_POSITION], 1, &lightPosition_cameraspace.x);
+	}
+
+	modelStack.PushMatrix();
+	modelStack.Rotate(90, 0, 1, 0);
+	RenderMesh(meshList[GEO_SETTINGMENU], false);
+
+		modelStack.PushMatrix();
+		modelStack.Translate(0.f, 15.f, 1.f);
+		modelStack.Scale(0.7f, 0.25f, 0.f);
+		RenderMesh(meshList[GEO_SETTING], false);
+		modelStack.PopMatrix();
+
+		modelStack.PushMatrix();
+		modelStack.Translate(-10.f, pos, 1.f);
+		modelStack.Scale(0.2f, 0.2f, 0.f);
+		RenderMesh(meshList[GEO_POINTER], false);
+		modelStack.PopMatrix();
+
+		modelStack.PushMatrix();
+		modelStack.Translate(0.f, 7.f, 1.f);
+		modelStack.Scale(0.6f, 0.2f, 0.f);
+		RenderMesh(meshList[GEO_AUDIOBUTTON], false);
+
+			modelStack.PushMatrix();
+			modelStack.Translate(0.f, -18.f, 0.f);
+			modelStack.Scale(0.6f, 0.8f, 0.f);
+			RenderMesh(meshList[GEO_BUTTON1], false);
+			modelStack.PopMatrix();
+	
+		modelStack.PopMatrix();
+
+		modelStack.PushMatrix();
+		modelStack.Translate(0.f, -1.f, 1.f);
+		modelStack.Scale(0.6f, 0.2f, 0.f);
+		RenderMesh(meshList[GEO_AUDIOBUTTON], false);
+
+			modelStack.PushMatrix();
+			modelStack.Translate(0.f, -18.f, 0.f);
+			modelStack.Scale(0.6f, 0.8f, 0.f);
+			RenderMesh(meshList[GEO_BUTTON2], false);
+			modelStack.PopMatrix();
+
+		modelStack.PopMatrix();
+
+		modelStack.PushMatrix();
+		modelStack.Translate(0.f, -9.f, 1.f);
+		modelStack.Scale(0.6f, 0.2f, 0.f);
+		RenderMesh(meshList[GEO_AUDIOBUTTON], false);
+
+			modelStack.PushMatrix();
+			modelStack.Translate(0.f, -18.f, 0.f);
+			modelStack.Scale(0.6f, 0.8f, 0.f);
+			RenderMesh(meshList[GEO_BUTTON3], false);
+			modelStack.PopMatrix();
+
+		modelStack.PopMatrix();
+
+
+	modelStack.PopMatrix();
+}
+
+void SceneSetting::Exit()
+{
+	// Cleanup here
+	for (int i = 0; i < NUM_GEOMETRY; ++i)
+	{
+		if (meshList[i] != NULL)
+			delete meshList[i];
+	}
+	// Cleanup VBO here
+	glDeleteVertexArrays(1, &m_vertexArrayID);
+	glDeleteProgram(m_programID);
+
+}
+
+
+void SceneSetting::RenderMesh(Mesh * mesh, bool enableLight)
+{
+	Mtx44 MVP, modelView, modelView_inverse_transpose;
+
+	MVP = projectionStack.Top() * viewStack.Top() * modelStack.Top();
+	glUniformMatrix4fv(m_parameters[U_MVP], 1, GL_FALSE, &MVP.a[0]);
+
+	modelView = viewStack.Top() * modelStack.Top();
+	glUniformMatrix4fv(m_parameters[U_MODELVIEW], 1, GL_FALSE, &modelView.a[0]);
+
+
+	if (enableLight)
+	{
+		glUniform1i(m_parameters[U_LIGHTENABLED], 1);
+		modelView_inverse_transpose = modelView.GetInverse().GetTranspose();
+		glUniformMatrix4fv(m_parameters[U_MODELVIEW_INVERSE_TRANSPOSE], 1, GL_FALSE, &modelView_inverse_transpose.a[0]);
+
+		//load material
+		glUniform3fv(m_parameters[U_MATERIAL_AMBIENT], 1, &mesh->material.kAmbient.r);
+		glUniform3fv(m_parameters[U_MATERIAL_DIFFUSE], 1, &mesh->material.kDiffuse.r);
+		glUniform3fv(m_parameters[U_MATERIAL_SPECULAR], 1, &mesh->material.kSpecular.r);
+		glUniform1f(m_parameters[U_MATERIAL_SHININESS], mesh->material.kShininess);
+	}
+	else
+	{
+		glUniform1i(m_parameters[U_LIGHTENABLED], 0);
+	}
+
+	if (mesh->textureID > 0) {
+		glUniform1i(m_parameters[U_COLOR_TEXTURE_ENABLED], 1);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, mesh->textureID);
+		glUniform1i(m_parameters[U_COLOR_TEXTURE], 0);
+	}
+	else {
+		glUniform1i(m_parameters[U_COLOR_TEXTURE_ENABLED], 0);
+	}
+	mesh->Render(); //this line should only be called once in the whole function
+
+	if (mesh->textureID > 0) glBindTexture(GL_TEXTURE_2D, 0);
+}
